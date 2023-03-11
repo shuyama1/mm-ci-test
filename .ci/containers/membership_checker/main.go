0dc6ca7d854f331cd366b2d0698845cf0eb04eed
Shuya Ma <shuyama@google.com>
2023-03-11 12:52:22 -0800

    dockerfile setup

--- .ci/containers/membership_checker/main.go
+++ .ci/containers/membership_checker/main.go
@@ -5,15 +5,13 @@ import (
 	"os"
 )
 
-
 func main() {
-	fmt.Println("check if PR author is trusted user")
 	GITHUB_TOKEN, ok := os.LookupEnv("GITHUB_TOKEN")
 	if !ok {
 		fmt.Println("Did not provide GITHUB_TOKEN environment variable")
 		os.Exit(1)
 	}
-	if (len(os.Args) <= 3) {
+	if len(os.Args) <= 3 {
 		fmt.Println("Not enough arguments")
 		os.Exit(1)
 	}
@@ -28,14 +26,14 @@ func main() {
 	fmt.Println("Commit SHA: ", commitSha)
 
 	author, err := getPullRequestAuthor(prNumber, GITHUB_TOKEN)
-	if err != nil{
+	if err != nil {
 		fmt.Println(err)
 		return
 	}
 
 	if target == "auto_run_tests" {
 		err = reviewerAssignment(author, prNumber, GITHUB_TOKEN)
-		if err != nil{
+		if err != nil {
 			fmt.Println(err)
 			return
 		}
@@ -43,9 +41,9 @@ func main() {
 
 	trusted := isTrustedUser(author, GITHUB_TOKEN)
 
-	if (target == "auto_run_tests" && trusted) || (target == "check_community_contributor" && !trusted){
+	if (target == "auto_run_tests" && trusted) || (target == "check_community_contributor" && !trusted) {
 		err = triggerMMPresubmitRuns("shuya-terraform-test", "mm-ci-test", commitSha)
-		if err != nil{
+		if err != nil {
 			fmt.Println(err)
 			return
 		}
